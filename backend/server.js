a732aa5f0b42b37334495a93b3f41582a226a5d6
yasht-007 <yashd2d@gmail.com>
2023-03-26 19:30:14 +0530

    cors issue

--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,7 @@ const deployContract = require("./config/deploy");
 
 dotenv.config();
 connectDB();
-// app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-})
+app.use(cors());
 app.use(express.json());
 
 app.get("/", function (req, res) {
